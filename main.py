49bdbe61f6bbc0a8d80ab6941dd8598d74d135e9
Cesar Villegas <99771832+cesarvillegase@users.noreply.github.com>
2023-11-21 19:48:16 -0600

    Update main

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from tracker import*
 import cvzone
 
 
-model=YOLO('yolov8s.pt')
+model=YOLO('yolov8n.pt')
 
 
 
@@ -16,26 +16,29 @@ def RGB(event, x, y, flags, param):
   
         
 
-cv2.namedWindow('RGB')
-cv2.setMouseCallback('RGB', RGB)
-cap=cv2.VideoCapture('vidp.mp4')
+cv2.namedWindow('Counter')
+cv2.setMouseCallback('Counter', RGB)
+cap=cv2.VideoCapture('video.mp4')
 
 
 my_file = open("coco.txt", "r")
+#my_file = open("labels.txt", "r")
 data = my_file.read()
 class_list = data.split("\n") 
 #print(class_list)
 
 count=0
-persondown={}
+person_entering={}
 tracker=Tracker()
 counter1=[]
 
 personup={}
 counter2=[]
-cy1=194
-cy2=220
+cx1 = 480 
+cx2 = 400 
 offset=6
+
+
 while True:    
     ret,frame = cap.read()
     if not ret:
@@ -77,13 +80,33 @@ while True:
         cy=int(y3+y4)//2
         cv2.circle(frame,(cx,cy),4,(255,0,255),-1)
         
-    cv2.line(frame,(3,194),(1018,194),(0,255,0),2)
-    cv2.line(frame,(5,220),(1019,220),(0,255,255),2)
-   
+        if cx1 < (cx + offset) and  cx1 > (cx - offset):
+            cv2.rectangle(frame, (x3, y3), (x4,y4), (0,0,255),2)
+            cvzone.putTextRect(frame,f'{id}', (x3, y3),1,2)
+            person_entering[id] = (cx, cy)
+             
+        if id in person_entering:
+            if cx2 < (cx + offset) and  cx2 > (cx - offset):
+                cv2.rectangle(frame, (x3, y3), (x4,y4), (0,255,255),2)
+                cvzone.putTextRect(frame,f'{id}', (x3, y3),1,2)
+                if counter1.count(id) == 0:
+                    counter1.append(id)
+            
+    
+    #cv2.line(frame, (start_x, start_y), (end_x, end_y), (B, G, R), thickness)
+    
+    # Vertical Line
+    cv2.line(frame,(cx1, 0),(cx1,1080),(0,255,0),2) # Green
+    cv2.line(frame,(cx2, 0),(cx2,1080),(0,255,255),2) # Yellow
+    
+    # Horizontal line
+    #cv2.line(frame,(0, cy1),(1080,cy1),(0,255,0),2) # Green
+    #cv2.line(frame,(0, cy2),(1080,cy2),(0,255,255),2) # Yellow
 
-    cv2.imshow("RGB", frame)
+    entering = (len(counter1))
+    cvzone.putTextRect(frame, f'Person {entering}', (50, 60), 2, 2)
+    cv2.imshow("Counter", frame)
     if cv2.waitKey(1)&0xFF==27:
         break
 cap.release()
 cv2.destroyAllWindows()
-
